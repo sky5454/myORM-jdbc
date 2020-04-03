9aed1805649fd1f6b80e515f34b8f1381e4927fd
zheng545475197 <sky5454@users.noreply.github.com>
2020-04-03 16:53:20 +0800

    Refactor:  optimize code and fixed bugs; Add:  support no parm.

--- src/util/dynproxy/MapperInvoHander.java
+++ src/util/dynproxy/MapperInvoHander.java
@@ -1,15 +1,11 @@
 package util.dynproxy;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import util.DBConnecter;
-import util.handleErr;
 
 
 public class MapperInvoHander implements InvocationHandler {
@@ -17,44 +13,13 @@ public class MapperInvoHander implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String sqlString = ClassUtil.getValue(method);
-        Object[] values = args.clone();
-
-        // TODO: add handler for Entity
-        if (sqlString.contains("?E")) {
-            Field[] fields = args[0].getClass().getDeclaredFields();
-            int entitySize = fields.length;
-            String parmString = "";
-            values = new Object[entitySize];
-            for (int i = 0; i < entitySize; i++) {
-                parmString = (i == entitySize-1)? parmString.concat("\\?") : parmString.concat("\\?\\, ");
-                boolean accessFlag = fields[i].isAccessible();
-                fields[i].setAccessible(true);
-                values[i] = fields[i].get(args[0]);
-                fields[i].setAccessible(accessFlag);
-            }
-            sqlString = sqlString.replaceAll("\\?E", parmString);
-            System.out.println("Found Entity and has concated!");
+        PreparedStatement pStatement;
+        if (args == null) {
+            pStatement = DBConnecter.getConnection().prepareStatement(sqlString);
+        } else {
+            sqlString = pStatementHelper.concatPreSqlString(sqlString, args[0]);
+            pStatement = pStatementHelper.parseParamer(sqlString, args, method);
         }
-
-        // exec -> sql
-        Connection conn = DBConnecter.getConnection();
-        try {
-            PreparedStatement pStatement = conn.prepareStatement(sqlString);
-            int paramCount = pStatement.getParameterMetaData().getParameterCount();
-            System.out.println("### prepareStatement: " + sqlString);
-            System.out.print("### Params: " + '\t');
-            
-            // set Entity args to pStatement
-            for (int i = 0; i < paramCount; i++) {
-                // TODO: NOTE may be 90 convert to "B"?
-                pStatement.setObject(i+1, values[i]);                                                                          // set:  void com.mysql.cj.AbstractQueryBindings.setObject(int parameterIndex, Object parameterObj)
-                String valueString = (values[i] == null)? "null" : values[i].toString();
-                System.out.print(valueString + "\t | \t");
-                sqlString = sqlString.replaceFirst("\\?", valueString);
-            }
-            System.out.println();
-            System.out.println("guessSql: " + sqlString);
-
 
             if (sqlString.toLowerCase().contains("select")) {       // SELECT
                 pStatement.execute();
@@ -72,23 +37,9 @@ public class MapperInvoHander implements InvocationHandler {
                 else
                     return pStatement.executeUpdate();
             }
-           
-
-
-        } catch (SQLException e) {
-            handleErr.printErr(e, method, sqlString, "SQLException", false);
-        }
        
-        
-
-
-        String returnType = method.getGenericReturnType().getTypeName();
-        System.out.println(" -[ReturnTYPE]: " + returnType);        // int, java.lang.String  etc..
-        switch (returnType) {
-            
-        }
-        
-        return sqlString.length();
+        // System.out.println(" -[ReturnTYPE]: " + method.getGenericReturnType().getTypeName());        // int, java.lang.String  etc..
+ 
     }
     
     
--- src/util/handleErr.java
+++ src/util/handleErr.java
@@ -6,7 +6,7 @@ public class handleErr {
     private static int i;
 
     public static void printErr(Exception e, String msg, boolean enablePrintStack) {
-        System.err.println("=====================================================[E"+  i++ +"]=====================================================");
+        System.err.println("\n=====================================================[E"+  i++ +"]=====================================================");
         System.err.println("[ERR]:  \t\t" + msg);
         // e.getCause()
         System.err.println("[ERR_MSG] " + e.getClass().getName() + ":  " + e.getMessage());
@@ -21,7 +21,7 @@ public class handleErr {
 
     public static void printErr(Exception e, Method method, String sqlString, String msg, boolean enablePrintStack) {
         // TODO: 放到ErrHandler
-        System.err.println("=====================================================[E"+  i++ +"]=====================================================");
+        System.err.println("\n=====================================================[E"+  i++ +"]=====================================================");
         System.out.println( "\n\t" +
                             "[ERR]: \t\t" + msg +
                             "[ERR_MSG]:" + e.getMessage()  + "\n\t" +
